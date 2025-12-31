41bafc14db8fe2382f18c9f7eca14f5f7d8e6372
dismint <mintjjc@gmail.com>
2025-12-30 23:17:47 -0500

    split file

---
+++ internal/interact/view.go
@@ -0,0 +1,51 @@
+package interact
+
+import (
+	"fmt"
+
+	"github.com/dismint/dispass/internal/state"
+	"github.com/dismint/dispass/internal/uconst"
+)
+
+func (m *Model) viewViewport(sm *state.Model) string {
+	_, _, exists := m.getSelectedCredInfo(sm)
+	if !exists && m.mode != ModeViewport {
+		return "Feeling empty, create new credentials?"
+	}
+	return fmt.Sprintf("%v\n%v\n%v",
+		m.viewportSourceInput.View(),
+		m.viewportUsernameInput.View(),
+		m.viewportPasswordInput.View(),
+	)
+}
+
+func (m *Model) View(sm *state.Model) string {
+	start, end := m.resultPaginator.GetSliceBounds(len(m.topIDs))
+	topPageIDs := m.topIDs[start:end]
+
+	var resultList string
+	for locOnPage, topID := range topPageIDs {
+		prefix := " "
+		if locOnPage == m.resultLocOnPage {
+			prefix = uconst.SymbolStyle.Render(">")
+		}
+		resultList += fmt.Sprintf("%v %v %v\n",
+			prefix,
+			uconst.TruncAndPadListElem(sm.KeyToCredInfo[topID].Source),
+			uconst.TruncAndPadListElem(sm.KeyToCredInfo[topID].Username),
+		)
+	}
+	if resultList == "" {
+		resultList = "No Results Found"
+	}
+
+	view := fmt.Sprintf("%v\n\n%v\n\n%v\n\n%v\n\n%v",
+		m.helpModel.View(m.keyMap),
+		m.keyInput.View(),
+		uconst.ViewportViewStyle.Render(m.viewViewport(sm)),
+		m.resultPaginator.View(),
+		resultList,
+	)
+
+	return uconst.ViewStyle.Render(view)
+}
