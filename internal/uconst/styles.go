a3b036f4922820fe752f0ba1156f60ff8577c277
dismint <mintjjc@gmail.com>
2026-01-11 12:46:30 -0500

    change colors

--- internal/uconst/styles.go
+++ internal/uconst/styles.go
@@ -12,39 +12,51 @@ var (
 	PaginatorDotString = "▪"
 )
 
-// https://lospec.com/palette-list/seafoam
-const seafoam1 = "#37364e"
-const seafoam2 = "#355d69"
-const seafoam3 = "#6aae9d"
-const seafoam4 = "#b9d4b4"
-const seafoam5 = "#f4e9d4"
-const seafoam6 = "#d0baa9"
-const seafoam7 = "#9e8e91"
-const seafoam8 = "#5b4a68"
-
-const lpred = "#f8858b"
+// https://lospec.com/palette-list/lost-century
+const (
+	lostCentury1  = "#d1b187"
+	lostCentury2  = "#c77b58"
+	lostCentury3  = "#ae5d40"
+	lostCentury4  = "#79444a"
+	lostCentury5  = "#4b3d44"
+	lostCentury6  = "#ba9158"
+	lostCentury7  = "#927441"
+	lostCentury8  = "#4d4539"
+	lostCentury9  = "#77743b"
+	lostCentury10 = "#b3a555"
+	lostCentury11 = "#d2c9a5"
+	lostCentury12 = "#8caba1"
+	lostCentury13 = "#4b726e"
+	lostCentury14 = "#574852"
+	lostCentury15 = "#847875"
+	lostCentury16 = "#ab9b8e"
+)
 
 var (
 	SymbolStyle = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
-		Light: seafoam1,
-		Dark:  seafoam3,
+		Light: lostCentury10,
+		Dark:  lostCentury12,
 	})
 	TextStyle = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
-		Light: seafoam1,
-		Dark:  seafoam5,
+		Light: lostCentury10,
+		Dark:  lostCentury11,
 	})
 	HelpKeyStyle = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
-		Light: seafoam7,
-		Dark:  seafoam6,
+		Light: lostCentury10,
+		Dark:  lostCentury15,
 	})
 	HelpDescStyle = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
-		Light: seafoam7,
-		Dark:  seafoam7,
+		Light: lostCentury10,
+		Dark:  lostCentury16,
 	})
 	HelpSeparatorStyle = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
-		Light: seafoam7,
-		Dark:  seafoam8,
+		Light: lostCentury10,
+		Dark:  lostCentury14,
 	})
+	BorderColor = lipgloss.AdaptiveColor{
+		Light: lostCentury10,
+		Dark:  lostCentury12,
+	}
 )
 
 var HelpStyles = help.Styles{
@@ -59,19 +71,19 @@ var HelpStyles = help.Styles{
 var (
 	ViewStyle         = lipgloss.NewStyle().Padding(1, 2).Width(50)
 	ViewportViewStyle = lipgloss.NewStyle().Padding(0, 1).Width(44).
-				Border(lipgloss.RoundedBorder())
+				Border(lipgloss.RoundedBorder()).BorderForeground(BorderColor)
 	MessageBaseStyle       = lipgloss.NewStyle().Padding(0, 1)
 	MessageLevelErrorStyle = MessageBaseStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: seafoam7,
-		Dark:  lpred,
+		Light: lostCentury10,
+		Dark:  lostCentury2,
 	})
 	MessageLevelSuccessStyle = MessageBaseStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: seafoam7,
-		Dark:  seafoam4,
+		Light: lostCentury10,
+		Dark:  lostCentury12,
 	})
 	MessageLevelNotifStyle = MessageBaseStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: seafoam7,
-		Dark:  seafoam5,
+		Light: lostCentury10,
+		Dark:  lostCentury13,
 	})
 )
 
@@ -87,5 +99,5 @@ func NewTextInput(prompt string) textinput.Model {
 
 func TruncAndPadListElem(text string) string {
 	truncText := truncate.StringWithTail(text, 20, "…")
-	return lipgloss.NewStyle().Width(21).Render(truncText)
+	return TextStyle.Width(21).Render(truncText)
 }
